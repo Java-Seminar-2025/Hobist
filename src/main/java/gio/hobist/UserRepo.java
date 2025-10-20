b0339d63aff53471f54359503c39072a76bb70a3
Marko Grubisic <markogrubisic2@gmail.com>
2025-10-20 13:13:51 +0200

    added src code with working signup (you can add name,surname,email and password  to database)

---
+++ src/main/java/gio/hobist/UserRepo.java
@@ -0,0 +1,11 @@
+package gio.hobist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+
+@Repository
+public interface UserRepo extends JpaRepository<User,Long> {
+}
