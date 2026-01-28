c04f2686b1953b114a8905486074c972963d223f
Marko Grubisic <markogrubisic2@gmail.com>
2026-01-28 20:46:24 +0100

    added comment section and comment submit

--- src/main/java/gio/hobist/Controller/postController.java
+++ src/main/java/gio/hobist/Controller/postController.java
@@ -1,17 +1,16 @@
 package gio.hobist.Controller;
 
+import gio.hobist.Dto.CommentDto;
 import gio.hobist.Dto.ContentLikeDto;
 import gio.hobist.Entity.ContentLike;
+import gio.hobist.Service.CommentService;
 import gio.hobist.Service.PostService;
 import gio.hobist.Service.UserService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -22,6 +21,7 @@ public class postController {
 
     private final UserService userService;
     private final PostService postService;
+    private final CommentService commentService;
 
     @GetMapping("/{postId}")
     public String post(@PathVariable UUID postId, Model model, HttpSession session){
@@ -51,5 +51,23 @@ public class postController {
        postService.createLike(contentLikeDto);
 
         return "redirect:/post/" + postId;
+    }
+    @PostMapping("/{postId}/submitComment")
+    public String submitComment(@PathVariable UUID postId,HttpSession session,@ModelAttribute(name = "comment") String comment) {
+
+        var userId=(UUID)session.getAttribute("userId");
+
+       var commentDto=new CommentDto(
+               null,
+               postId,
+               userId,
+               null,
+               comment,
+               null
+       );
+
+      commentService.createComment(commentDto);
+
+        return "redirect:/post/"+ postId;
     }
 }
